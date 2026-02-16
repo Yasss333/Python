7f32aaf9f5071954016e8fd54e8069897572c274
Yasss333 <yashmandhare3@gmail.com>
2026-02-16 11:42:01 +0530

    basics fo type and variable done

---
+++ 01-PY/samole1.py
@@ -0,0 +1,42 @@
+# color =input("WHich Color : ")
+
+# if(color=="red" or color=='blue'):
+#     print("Correct")
+# else:
+#     print('False')
+
+
+
+# Ternary Operato
+
+# food=input("What food : ")
+
+# print(food) if food=="oats" else print("Wrong");       
+
+# type="healthy" if food=='oats' or food=="apple" else "junk";
+
+# print(type)
+
+
+age=10
+
+# print("Yes for vote ") if age>=18 else print("NOT allowed")
+
+'''  
+
+
+    allowed=("no","yes")[age>=18]
+else:
+    print("You can vote")
+    print("Not alowed")
+if(allowed=="yes"):
+'''
+
+
+age="yash"
+a=4 
+b=3
+sum=10
+sum+=a
+# print(age)
+print(sum)
---
+++ 01-PY/sample.py
@@ -0,0 +1,10 @@
+age=int(input('What is your age : '))
+# float=float(input('What is your price : '))
+# print("Price  : ",float)
+
+
+if(age<18):
+    print("YOUNG")
+else:
+    print("OLD")
+
